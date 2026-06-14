78742c5b499508b19fcbd4beaf74435f6fc5605d
Fernando Carta <cartafernando@outlook.com>
2026-06-14 16:42:21 -0500

    Entidad: Categoria

--- src/main/java/entity/Categoria.java
+++ src/main/java/entity/Categoria.java
@@ -1,4 +1,37 @@
 package entity;
 
+import java.util.UUID;
+
 public class Categoria {
+
+
+    private UUID idCategoria;
+    private String nombre;
+
+    public Categoria(){}
+
+    public Categoria(UUID idCategoria, String nombre) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+    }
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public UUID getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(UUID idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
